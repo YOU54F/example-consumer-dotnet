43be343ac071497f82258c2fcb2def7070cb51ee
Yousaf Nabi <yousafn@gmail.com>
2025-03-03 17:00:56 +0000

    test: new field

--- tests/ConsumerPactTests.cs
+++ tests/ConsumerPactTests.cs
@@ -24,7 +24,7 @@ namespace tests
 
             products = new List<object>()
             {
-                new { id = "27", name = "burger", type = "food" }
+                new { id = "27", name = "burger", type = "food", "test": "new field" }
             };
 
             var Config = new PactConfig
